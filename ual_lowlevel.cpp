2e5b1b4adfd7412f1d14fd187f3c3e49c43b6cf9
Olivier Hoenen <olivier.hoenen@ipp.mpg.de>
2018-06-25 16:11:21 +0200

    Fix returned errors in valid nodata exceptions case at the lowlevel

--- ual_lowlevel.cpp
+++ ual_lowlevel.cpp
@@ -595,8 +595,8 @@ int ual_read_data(int ctxID, const char *field, const char *timebase,
   catch (const UALNoDataException e) {
     //std::cout << e.what() << " *recoverable* \n";
     Lowlevel::setDefaultValue(datatype, dim, data, size);
-    //status = 0;
-    status = ualerror::nodata_err; //<=== if different mode nodata not recoverable
+    status = 0;
+    //status = ualerror::nodata_err; //<=== if different mode nodata not recoverable
   }
   catch (const UALBackendException e) {
     std::cout << "ual_read_data: " << e.what() << "\n";
