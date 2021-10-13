1d18b197d0903a140c51a0dbb1dedf8dda45555b
Ludovic Fleury <ludovic.fleury@cea.fr>
2021-10-13 10:47:30 +0200

    Adding access_layer_plugin.h

---
+++ access_layer_plugin.h
@@ -0,0 +1,27 @@
+#ifndef AL_PLUGIN_H
+#define AL_PLUGIN_H 1
+
+#include "ual_const.h"
+#include "ual_lowlevel.h"
+
+
+class access_layer_plugin {
+   public:
+    access_layer_plugin() {}
+    virtual ~access_layer_plugin() {}
+
+    virtual void begin_global_action(int pulseCtx, const char* dataobjectname, int mode, int opCtx) = 0;
+    virtual void begin_slice_action(int pulseCtx, const char* dataobjectname, int mode, double time, int interp, int opCtx) = 0;
+    virtual void begin_arraystruct_action(int ctx, const char* fieldPath, const char* timeBasePath, int arraySize) = 0;
+
+    virtual int read_data(int ctx, const char* fieldPath, const char* timeBasePath, void **data, int datatype, int dim, int *size) = 0;
+
+    //virtual void write_aos_content(int ctx, const char* fieldPath, const char* timeBasePath) = 0;
+    //virtual void close_pulse(int pulseCtx, int mode) = 0;
+
+};
+
+typedef access_layer_plugin* create_t();
+typedef void destroy_t(access_layer_plugin*);
+
+#endif
