f2e01b2ca2c1c0a92aab8227d0d11049f94d37c4
Jonathan Hollocombe <jonathan.hollocombe@ukaea.uk>
2022-10-31 15:12:20 +0000

    Updating UDA backend cache to unpack capnp serialised data from UDA.

--- uda_cache.cpp
+++ uda_cache.cpp
@@ -1,58 +1,54 @@
 #include "uda_cache.hpp"
 
 #include <algorithm>
+#include <clientserver/udaTypes.h>
 
 #include "ual_const.h"
 #include "uda_exceptions.hpp"
 
-void imas::uda::add_node_to_cache(::uda::TreeNode& node, CacheType& cache)
+void imas::uda::add_node_to_cache(NodeReader* node, CacheType& cache)
 {
-    auto data = node.atomicVector("value");
+    const char* name = uda_capnp_read_name(node);
+    int type = uda_capnp_read_type(node);
+    size_t rank = uda_capnp_read_rank(node).value;
 
-    if (data.type() == typeid(unsigned char)) {
-        auto type = node.atomicScalar("datatype");
-        auto itype = type.as<int>();
+    std::vector<size_t> shape(rank);
+    uda_capnp_read_shape(node, shape.data());
 
-        auto rank = node.atomicScalar("rank");
-        auto irank = rank.as<int>();
+    size_t count = std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<size_t>());
+    std::vector<char> bytes(count);
 
-        auto dims = node.atomicVector("dims");
-        auto idims = dims.as<int>();
-        idims.resize(irank);
-
-        auto name = node.atomicScalar("name");
-        std::string sname = name.as<char*>();
-
-        auto vec = data.as<unsigned char>();
-        switch (itype) {
-            case CHAR_DATA:
-                add_value_to_cache<char>(sname, vec, idims, cache);
-                break;
-            case INTEGER_DATA:
-                add_value_to_cache<int>(sname, vec, idims, cache);
-                break;
-            case DOUBLE_DATA:
-                add_value_to_cache<double>(sname, vec, idims, cache);
-                break;
-            case COMPLEX_DATA:
-                add_value_to_cache<double _Complex>(sname, vec, idims, cache);
-                break;
-            case 0:
-                // no data - ignore
-                break;
-            default: throw imas::uda::CacheException("Unknown data type: " + std::to_string(itype));
-        }
+    switch (type) {
+        case CHAR_DATA:
+            add_value_to_cache<char>(name, node, shape, cache);
+            break;
+        case INTEGER_DATA:
+            add_value_to_cache<int>(name, node, shape, cache);
+            break;
+        case DOUBLE_DATA:
+            add_value_to_cache<double>(name, node, shape, cache);
+            break;
+        case COMPLEX_DATA:
+            add_value_to_cache<double _Complex>(name, node, shape, cache);
+            break;
+        case 0:
+            // no data - ignore
+            break;
+        default: throw imas::uda::CacheException("Unknown data type: " + std::to_string(type));
     }
 }
 
 void imas::uda::add_data_to_cache(const ::uda::Result& result, CacheType& cache)
 {
-    if (result.errorCode() == 0 && result.isTree()) {
-        auto tree = result.tree();
-        auto data_node = tree.child(0);
+    if (result.errorCode() == 0 && result.uda_type() == UDA_TYPE_CAPNP) {
+        const char* data = result.raw_data();
+        size_t size = result.size();
+        auto tree = uda_capnp_deserialise(data, size);
 
-        for (int n = 0; n < data_node.numChildren(); ++n) {
-            auto child = data_node.child(n);
+        auto root = uda_capnp_read_root(tree);
+        size_t num_children = uda_capnp_num_children(root);
+        for (size_t n = 0; n < num_children; ++n) {
+            auto child = uda_capnp_read_child_n(tree, root, n);
             add_node_to_cache(child, cache);
         }
     }
--- uda_cache.hpp
+++ uda_cache.hpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <numeric>
 #include <UDA.hpp>
+#include <serialisation/capnp_serialisation.h>
 
 namespace imas {
 namespace uda {
@@ -16,25 +17,25 @@ namespace uda {
 using VariantVector = boost::variant<std::vector<int>, std::vector<double>, std::vector<char>, std::vector<double _Complex>>;
 
 struct CacheData {
-    std::vector<int> shape;
+    std::vector<size_t> shape;
     VariantVector values;
 };
 
 using CacheType = std::map<std::string, imas::uda::CacheData>;
 
 template <typename T>
-void add_value_to_cache(const std::string& name, const std::vector<unsigned char>& bytes, const std::vector<int>& shape, imas::uda::CacheType& cache)
+void add_value_to_cache(const std::string& name, NodeReader* node, const std::vector<size_t>& shape, imas::uda::CacheType& cache)
 {
     size_t count = std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<size_t>());
 
-    std::vector<T> d = {};
-    auto buffer = reinterpret_cast<const T *>(bytes.data());
-    d.resize(count);
-    std::copy(buffer, buffer + count, d.begin());
-    cache[name] = CacheData{ shape, d };
+    std::vector<T> data(count);
+    auto buffer = reinterpret_cast<char*>(data.data());
+
+    uda_capnp_read_data(node, buffer);
+    cache[name] = CacheData{ shape, data };
 }
 
-void add_node_to_cache(::uda::TreeNode& node, CacheType& cache);
+void add_node_to_cache(NodeReader* node, CacheType& cache);
 void add_data_to_cache(const ::uda::Result& result, CacheType& cache);
 
 }
