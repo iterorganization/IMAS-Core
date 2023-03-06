b5a4be0ce8a1f61d005ebba8204fda425bb922b0
Ludovic FLEURY <ludovic.fleury@cea.fr>
2023-03-06 13:45:11 +0100

    Adding file readback_plugin_feature.h

---
+++ readback_plugin_feature.h
@@ -0,0 +1,21 @@
+#ifndef READBACK_PLUGIN_FEATURE_H
+#define READBACK_PLUGIN_FEATURE_H 1
+
+#include <string>
+
+class readback_plugin_feature {
+
+    public:
+
+    readback_plugin_feature() {}
+    virtual ~readback_plugin_feature() {}
+
+    virtual std::string getReadbackName(const std::string &path, int *application_index) = 0;
+    virtual std::string getReadbackCommit(const std::string &path) = 0;
+    virtual std::string getReadbackVersion(const std::string &path) = 0;
+    virtual std::string getReadbackRepository(const std::string &path) = 0;
+    virtual std::string getReadbackParameters(const std::string &path) = 0;
+
+};
+
+#endif
