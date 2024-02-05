dc19502e3b4507e238a7f9c8e59eff5e85f9db11
Maarten Sebregts <msebregts@ignitioncomputing.com>
2024-02-05 12:47:32 +0100

    CI build script

---
+++ ci/build_and_test.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Bamboo CI script to build and run tests for all lowlevel components
+#
+# This script expects to be run from the repository root directory
+
+# Debuggging:
+set -e -o pipefail
+echo "Loading modules..."
+
+# Set up environment such that module files can be loaded
+. /usr/share/Modules/init/sh
+
+module purge
+module load CMake/3.18.4-GCCcore-10.2.0
+# Load modules required for building the lowlevel:
+module load \
+    Boost/1.74.0-GCCcore-10.2.0 \
+    HDF5/1.10.7-GCCcore-10.2.0-serial \
+    MDSplus/7.96.17-GCCcore-10.2.0 \
+    UDA/2.7.4-GCCcore-10.2.0
+
+# Load modules required for building MDSplus models
+module load \
+    Saxon-HE/11.4-Java-11 \
+    MDSplus-Java/7.96.17-GCCcore-10.2.0-Java-11
+
+# Load modules required for building the lowlevel python bindings
+module load \
+    Python/3.8.6-GCCcore-10.2.0 \
+    SciPy-bundle/2020.11-intel-2020b
+
+# Debuggging:
+echo "Done loading modules"
+set -x
+
+
+# Ensure the build directory is clean:
+rm -rf build
+
+# CMake configuration:
+CMAKE_ARGS=(
+    -D "CMAKE_INSTALL_PREFIX=$(pwd)/test-install/"
+    # Enable all backends
+    -D AL_BACKEND_HDF5=ON
+    -D AL_BACKEND_MDSPLUS=ON
+    -D AL_BACKEND_UDA=ON
+    # Build MDSplus models
+    -D AL_BUILD_MDSPLUS_MODELS=ON
+    # "Download" dependencies from repos checked out by Bamboo in the repos/ folder:
+    -D AL_DOWNLOAD_DEPENDENCIES=ON
+    -D "AL_COMMON_GIT_REPOSITORY=$(pwd)/repos/al-common/"
+    -D "DD_GIT_REPOSITORY=$(pwd)/repos/data-dictionary/"
+    # DD version:
+    -D DD_VERSION=master/3
+)
+# Note: we don't set CC or CXX compiler, so CMake will pick the default (GCC) compilers
+cmake -B build "${CMAKE_ARGS[@]}"
+
+# Build
+make -C build -j8 all
+
+# Test install
+make -C build install
+
+# List installed files
+ls -lR install
