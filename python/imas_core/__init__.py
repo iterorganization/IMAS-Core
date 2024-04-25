7d4ae5d8e7f51e7da83ee3e3f29fc21c45a2cc58
Simon McIntosh <simon.mcintosh@iter.org>
2024-04-25 18:21:10 +0200

    include os.add_dll_directory guard for non "win_32" systems

--- python/imas_core/__init__.py
+++ python/imas_core/__init__.py
@@ -1,13 +1,17 @@
-"""Access Layer Core Python bindings.
-"""
+"""Access Layer Core Python bindings."""
 
-import os 
+import os
 import pathlib
 
-os.add_dll_directory(pathlib.Path(__file__).parents[1] / "imas_core.libs")
+if os.name == "nt":
+    os.add_dll_directory(pathlib.Path(__file__).parents[1] / "imas_core.libs")
 
 from . import _al_lowlevel
 from . import imasdef
 
 from ._version import version as __version__
 from ._version import version_tuple
+
+__all__ = ["_al_lowlevel", "imasdef", "__version__", "version_tuple"]
+
+del os, pathlib
