3132b2bf57e3c2898fdb5b781271355c1154b152
Simon McIntosh <simon.mcintosh@iter.org>
2024-05-03 09:38:21 +0200

    rename skbuild and vcpkg .cmake files

--- Vcpkg.cmake
+++
@@ -1,9 +0,0 @@
-# Configure CMAKE_TOOLCHAIN_FILE 
-option(VCPKG "Use VCPKG to manage third-party dependancies" OFF)
-if(VCPKG)
-  if(NOT DEFINED ENV{VCPKG_ROOT}) 
-    message(FATAL_ERROR "Use of VCPKG:ON requres VCPKG_ROOT environment variable to set CMAKE_TOOLCHAIN_FILE.")
-  endif()
-  set(CMAKE_TOOLCHAIN_FILE $ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
-endif()
-
