3ebab9c93445be2d31501bb5ec1c8767a5167c89
Simon McIntosh <simon.mcintosh@iter.org>
2024-05-06 12:40:10 +0200

    update unit-tests to improve code coverage

---
+++ python/tests/test_imasdef.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pytest
+
+import imas_core
+
+
+@pytest.mark.parametrize(
+    "name, value", (["EMPTY_INT", -999999999], ["EMPTY_FLOAT", -9e40])
+)
+def test_constants(name, value):
+    assert np.isclose(getattr(imas_core.imasdef, name), value)
+
+
+def test_DD_version():
+    assert imas_core.imasdef.DD_VERSION.decode() == "!!DEPRECATED!!"
+
+
+def test_AL_version():
+    AL_VERSION = imas_core.imasdef.AL_VERSION.decode()
+    assert (
+        tuple(int(num) for num in AL_VERSION.split(".")) == imas_core.version_tuple[:3]
+    )
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
--- python/tests/test_import.py
+++ python/tests/test_import.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pytest
 
 import imas_core
@@ -7,25 +6,6 @@ import imas_core
 @pytest.mark.parametrize("attribute", ["al_defs", "exception", "imasdef"])
 def test_modules(attribute):
     assert hasattr(imas_core, attribute)
-
-
-@pytest.mark.parametrize(
-    "name, value", (["EMPTY_INT", -999999999], ["EMPTY_FLOAT", -9e40])
-)
-def test_constants(name, value):
-    assert np.isclose(getattr(imas_core.imasdef, name), value)
-
-
-def test_DD_version():
-    assert imas_core.imasdef.DD_VERSION.decode() == "!!DEPRECATED!!"
-
-
-def test_AL_version():
-    AL_VERSION = imas_core.imasdef.AL_VERSION.decode()
-    assert (
-        tuple(int(num) for num in AL_VERSION.split("."))
-        == imas_core.version_tuple[:3]
-    )
 
 
 if __name__ == "__main__":
