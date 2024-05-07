0db8419df591804dc8391f8c06d474e53289652b
Simon McIntosh <simon.mcintosh@iter.org>
2024-05-07 14:45:11 +0200

    revert dist dir move

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -43,8 +43,8 @@ fi
 # Ensure that the install directory is clean:
 rm -rf test-install
 
-# Ensure the build and dist directories are clean:
-rm -rf build dist
+# Ensure the build directory is clean:
+rm -rf build 
 
 # CMake configuration:
 CMAKE_ARGS=(
@@ -85,7 +85,7 @@ module purge
 module load Python/3.11.5-GCCcore-13.2.0
 python3.11 -m venv build/pip_install 
 source build/pip_install/bin/activate 
-python3.11 -m pip install --find-links=dist imas-core[test,cov]
+python3.11 -m pip install --find-links=build/dist imas-core[test,cov]
 pytest --junitxml results.xml --cov imas_core --cov-report xml --cov-report html 
 coverage2clover -i coverage.xml -o clover.xml
 
