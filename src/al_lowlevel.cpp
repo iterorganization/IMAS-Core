32b94db4be4557421343f90239e0abfd394b8262
Aleksander Wiszniowski <awiszniowski@man.poznan.pl>
2024-05-14 10:40:52 +0200

    added overloading al_begin_dataentry_action to support char*

--- src/al_lowlevel.cpp
+++ src/al_lowlevel.cpp
@@ -802,8 +802,14 @@ al_status_t al_get_backendID(int ctxID, int *beid)
   return status;
 }
 
-
 al_status_t al_begin_dataentry_action(const char *uri, int mode, int *dectxID)
+{
+  std::string uri_str(uri);
+  al_begin_dataentry_action(uri_str, mode, dectxID);
+}
+
+
+al_status_t al_begin_dataentry_action(const std::string uri, int mode, int *dectxID)
 {
   al_status_t status = { 0 };
 
