84d00e3c6e35b8e9bc84fc8ba1d16f3978b55126
Simon McIntosh <simon.mcintosh@iter.org>
2024-05-23 18:05:21 +0200

    permit VCPKG to be set as env var

--- vcpkg.cmake
+++ vcpkg.cmake
@@ -1,5 +1,8 @@
 # Configure CMAKE_TOOLCHAIN_FILE 
 option(VCPKG "Use VCPKG to manage third-party dependancies" OFF)
+if(NOT VCPKG AND DEFINED ENV{VCPKG})
+  set(VCPKG $ENV{VCPKG})
+endif() 
 if(VCPKG)
   if(NOT DEFINED ENV{VCPKG_ROOT}) 
     message(FATAL_ERROR "Use of VCPKG:ON requres VCPKG_ROOT environment variable to set CMAKE_TOOLCHAIN_FILE.")
