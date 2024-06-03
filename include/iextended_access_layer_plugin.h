59495e4bfc41eeb1b87b923b8842dded07c2e20f
Ludovic Fleury <ludovic.fleury@cea.fr>
2024-06-03 08:21:25 +0200

    Revert "IMAS-3885: preserving ABI compatibility of the plugins interface (2)"
    This reverts commit 33a94f4fab4fc259afae0174214a3508ee16919c.

--- include/iextended_access_layer_plugin.h
+++
@@ -1,6 +0,0 @@
-// New interface class with the additional function
-class IExtendedAccessLayerPlugin {
-public:
-    virtual ~IExtendedAccessLayerPlugin() {}
-    virtual void begin_timerange_action(int pulseCtx, const char* dataobjectname, int mode, double tmin, double tmax, double dtime, int interp, int opCtx) = 0;
-};
