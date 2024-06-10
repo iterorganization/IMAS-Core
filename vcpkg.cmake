3e21b279c44cc4d614632e5c399acf4537218737
Simon McIntosh <simon.mcintosh@iter.org>
2024-06-10 13:43:47 +0200

    update VCPKG defaults for WIN32 OS

--- vcpkg.cmake
+++ vcpkg.cmake
@@ -1,8 +1,9 @@
-# Configure CMAKE_TOOLCHAIN_FILE 
-option(VCPKG "Use VCPKG to manage third-party dependancies" OFF)
 if(NOT VCPKG AND DEFINED ENV{VCPKG})
   set(VCPKG $ENV{VCPKG})
+elseif(NOT VCPKG AND WIN32)
+ set(VCPKG $ENV{VCPKG})
 endif() 
+
 if(VCPKG)
   if(NOT DEFINED ENV{VCPKG_ROOT}) 
     message(FATAL_ERROR "Use of VCPKG:ON requres VCPKG_ROOT environment variable to set CMAKE_TOOLCHAIN_FILE.")
