07dd6fc82b546ea64ab61e1a4d48b7eb8ac2d15d
Simon McIntosh <simon.mcintosh@iter.org>
2024-06-10 13:47:46 +0200

    move VCPKG option into dedicated vcpkg.cmake file

--- vcpkg.cmake
+++ vcpkg.cmake
@@ -3,10 +3,11 @@ if(NOT VCPKG AND DEFINED ENV{VCPKG})
 elseif(NOT VCPKG AND WIN32)
  set(VCPKG $ENV{VCPKG})
 endif() 
-
+option(VCPKG "Use VCPKG package to download and install 3rd party libraries" OFF)
 if(VCPKG)
   if(NOT DEFINED ENV{VCPKG_ROOT}) 
-    message(FATAL_ERROR "Use of VCPKG:ON requres VCPKG_ROOT environment variable to set CMAKE_TOOLCHAIN_FILE.")
+    message(FATAL_ERROR 
+    "Use of VCPKG:ON requres VCPKG_ROOT environment variable to set CMAKE_TOOLCHAIN_FILE.")
   endif()
   cmake_path(SET CMAKE_TOOLCHAIN_FILE $ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
 endif()
