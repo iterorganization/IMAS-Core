07042e401ff036c1b4199e83f155c119c4eea52b
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2024-11-25 11:05:01 +0100

    added typing-extensions back

--- common/cmake/ALBuildDocumentation.cmake
+++ common/cmake/ALBuildDocumentation.cmake
@@ -9,6 +9,7 @@ ln -sf '${al-common_SOURCE_DIR}/doc_common' '${CMAKE_CURRENT_SOURCE_DIR}/doc/'
 ln -sfT '${al-plugins_SOURCE_DIR}/' '${CMAKE_CURRENT_SOURCE_DIR}/doc/plugins'
 
 # Set up python venv
+module load typing-extensions
 ${Python3_EXECUTABLE} -m venv '${CMAKE_CURRENT_BINARY_DIR}/doc_venv'
 source '${CMAKE_CURRENT_BINARY_DIR}/doc_venv/bin/activate'
 pip install -r '${CMAKE_CURRENT_SOURCE_DIR}/doc/doc_common/requirements.txt'
